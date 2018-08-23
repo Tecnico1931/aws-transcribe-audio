c566068e97b6772b83e9f7ccad36366291047098
Connor Couetil <couetilc@gmail.com>
2018-08-23 14:45:55 -0400

    Fixed large upload file bug, expanded README

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# Creating an IAM User for the Lambda functions.
-# Verifying a Email Address for notifications.
-https://docs.aws.amazon.com/ses/latest/DeveloperGuide/verify-email-addresses-procedure.html
+# Amazon Web Services (AWS) Transcription Notification Software
+Automatically transcribe audio files added to an AWS S3 bucket and receive notification emails when the transcriptions are complete.
+
+## Table of Contents:
+
++ Description
++ Deployment Steps (Installation and use of software)
+..1. Creating an IAM User
+..2. Creating an S3 Bucket
+..3. Collecting Account Credentials
+..4. Running Deployment Script
+..5. Cleaning/Reverting Account Changes 
++ Tips to Monitor Functions
++ Documentation for Repository Files
+..+ credentials
+..+ climenthods
+..+ build
+..+ clean 
+..+ policies
+..+ transcribeAudio.js
+..+ extractTranscript.js
+
+## Description
+This software package is a collection of command line tools (cli tools) that create/manage roles, policies, and permissions for AWS Lambda functions. These functions may be used to easily start and format results from the AWS Transcribe service.  
+
+In order to install/use this functionality, [please follow the instructions in the "Deployment Steps" section.](./README.md#deployment-steps)
+
+The "transcribeAudio" function initiates the AWS Transcribe service whenever an audio file (mp3, mp4, wav, flac) is added to a user-specified S3 bucket.  AWS Transcribe then places the resulting transcription as a JSON in a user-created S3 bucket.  The "extractTranscript" function responds to this JSON file event, creating a formatted trascript and notifying a specified user by email.
+
+        S3          ->          Lambda              ->          Transcribe      ->          S3          ->      Lambda          ->          SNS/S3
+"upload audio file" ->  "start transcription job"   -> "transcribe audio file"  ->  "save JSON report"  ->  "format transcript" -> "broadcast formatted transcript"
+
+## Deployment Steps (Installation and use of software)
+## Tips to Monitor Functions
+## Documentation for Repository Files
--- extractTranscript.js
+++ extractTranscript.js
@@ -13,13 +13,15 @@ exports.parseJSON = function (event) {
     const extension = shortname.pop().toLowerCase();
     shortname = shortname.join('');
     const bucket = event.Records[0].s3.bucket.name;
-    const processed_on = filename.replace(/.json/ig, "").split('_').pop()
+    const processed_on = filename.replace(/.json$/ig, "").split('_').pop()
     const processed_file = filename.replace(/_[^_]*(?![^_]*_)/ig, "");
-    console.log(processed_on);
-    console.log(processed_file);
+
+    const destination_bucket = process.env.TRANSCRIPT_DESTINATION_S3BUCKET_NAME;
+    const textfilepath = `transcripts/${shortname}.txt`;
+    const htmlfilepath = `transcripts/${shortname}.html`;
 
     S3.getObject({
-        Bucket: bucket,
+        Bucket: destination_bucket,
         Key: filepath,
         ResponseContentType: 'application/json'
     }).promise()
@@ -30,13 +32,14 @@ exports.parseJSON = function (event) {
             <!DOCTYPE html>
             <html>
                 <body>
-                    <h1>Transcript for ${processed_file}</h1>
+                    <h1>Transcript for <span style="text-decoration:underline">${processed_file}</span></h1>
                     <p>${transcript}</p>
+                    <p>Processed on: ${processed_on}</p>
                     <p>
-                        <div>Processed on: ${processed_on}</div>
-                        <div>Path to transcript JSON: ${filepath}</div>
-                        <div>Path to transcript text: </div>
-                        <div>Path to transcript HTML: </div>
+                        <div><em>S3 bucket containing transcription results: ${destination_bucket}</em></div>
+                        <div><em>Path to JSON:</em> ${filepath}</div>
+                        <div><em>Path to text:</em> ${textfilepath}</div>
+                        <div><em>Path to HTML:</em> ${htmlfilepath}</div>
                     </p>
                 </body>
             </html>
@@ -44,23 +47,29 @@ exports.parseJSON = function (event) {
 
         S3.putObject({
             Body: transcript,
-            Bucket: process.env.TRANSCRIPT_DESTINATION_S3BUCKET_NAME,
+            Bucket: destination_bucket,
             Key: `transcripts/${shortname}.txt`,
             ContentType: 'text/plain'
-        });
+        }).promise()
+            .then(response => console.log(JSON.stringify(response)))
+            .catch(error => console.log(JSON.stringify(error)));
 
         S3.putObject({
             Body: html,
-            Bucket: process.env.TRANSCRIPT_DESTINATION_S3BUCKET_NAME,
+            Bucket: destination_bucket,
             Key: `transcripts/${shortname}.html`,
             ContentType: 'text/html'
-        });
+        }).promise()
+            .then(response => console.log(JSON.stringify(response)))
+            .catch(error => console.log(JSON.stringify(error)));
 
         SNS.publish({
             Subject: `Scribe completed: ${shortname}`,
             Message: html,
             TopicArn: process.env.SNS_TOPIC_ARN
-        });
+        }).promise()
+            .then(response => console.log(JSON.stringify(response)))
+            .catch(error => console.log(JSON.stringify(error)));
     })
     .catch(error => {
         console.log(error);
