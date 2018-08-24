e5ffeec32fc7e1dddd498761b4aee0560c860da9
Connor Couetil <couetilc@gmail.com>
2018-08-24 01:02:54 -0400

    Modified message format

--- extractTranscript.js
+++ extractTranscript.js
@@ -19,7 +19,6 @@ exports.parseJSON = function (event) {
     const destination_bucket = process.env.TRANSCRIPT_DESTINATION_S3BUCKET_NAME;
     const destination_bucket_url = `s3.console.aws.amazon.com/s3/buckets/${destination_bucket}`;
     const textfilepath = `transcripts/${shortname}.txt`;
-    const htmlfilepath = `transcripts/${shortname}.html`;
 
     S3.getObject({
         Bucket: destination_bucket,
@@ -29,22 +28,15 @@ exports.parseJSON = function (event) {
     .then(response => {
         const report = JSON.parse(response.Body.toString());
         const transcript = report.results.transcripts[0].transcript;
-        const html = `
-            <!DOCTYPE html>
-            <html>
-                <body>
-                    <h1>Transcript for <span style="text-decoration:underline">${processed_file}</span></h1>
-                    <p><em>Processed on: ${processed_on}</em></p>
-                    <p>
-                        <div><strong><a href="${destination_bucket_url}">S3 bucket containing transcription results: ${destination_bucket} (Link to bucket)</a></strong></div>
-                        <div><strong>Path to JSON:</strong> ${filepath}</div>
-                        <div><strong>Path to text:</strong> ${textfilepath}</div>
-                        <div><strong>Path to HTML:</strong> ${htmlfilepath}</div>
-                    </p>
-                    <p>${transcript}</p>
-                </body>
-            </html>
-        `;
+        const text = `Transcript for ${processed_file}
+
+Processed on: ${processed_on}
+S3 bucket containing transcription results: ${destination_bucket}
+Link to bucket: ${destination_bucket_url}
+Path to JSON: ${filepath}
+Path to text: ${textfilepath}
+
+${transcript}`;
 
         S3.putObject({
             Body: transcript,
@@ -55,18 +47,9 @@ exports.parseJSON = function (event) {
             .then(response => console.log(JSON.stringify(response)))
             .catch(error => console.log(JSON.stringify(error)));
 
-        S3.putObject({
-            Body: html,
-            Bucket: destination_bucket,
-            Key: `transcripts/${shortname}.html`,
-            ContentType: 'text/html'
-        }).promise()
-            .then(response => console.log(JSON.stringify(response)))
-            .catch(error => console.log(JSON.stringify(error)));
-
         SNS.publish({
             Subject: `Scribe completed: ${shortname}`,
-            Message: html,
+            Message: text,
             TopicArn: process.env.SNS_TOPIC_ARN
         }).promise()
             .then(response => console.log(JSON.stringify(response)))
